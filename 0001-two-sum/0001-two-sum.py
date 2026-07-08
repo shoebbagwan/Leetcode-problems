40261ec416fc72da7bcd1f701e44a3fec8a89e09
Shoeb Bagwan <148525073+shoebbagwan@users.noreply.github.com>
2026-07-08 17:05:31 +0530

    Time: 0 ms (100%), Space: 20.6 MB (7.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        n = len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i]+ nums[j] == target:
-                    return [i,j]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}  #val: index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return 
