866f7f457a075fa2611364bc12fef32688e49c8a
Shoeb Bagwan <148525073+shoebbagwan@users.noreply.github.com>
2026-09-16 11:00:35 +0530

    Time: 0 ms (100%), Space: 12.9 MB (95.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        n = len(nums)
-        for i in range (n):
-            for j in range(i+1 , n):
-                if nums[i] + nums[j] == target:
-                    return [i , j]          
+        seen  = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return[seen[complement],i]
+            seen[num]=i
