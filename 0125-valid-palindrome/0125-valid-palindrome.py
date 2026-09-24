83cb02b0b58145840530db0ef25455ca7f68cc9d
Shoeb Bagwan <148525073+shoebbagwan@users.noreply.github.com>
2026-09-24 20:35:31 +0530

    Time: 11 ms (92.43%), Space: 14.7 MB (9.02%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        cleaned = [c.lower() for c in s if c.isalnum()]
+        return cleaned == cleaned[::-1]
+        
